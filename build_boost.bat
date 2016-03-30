26e13987cf9c9025c0e9785ed9496c6668446677
Zsolt Parragi <zsolt.parragi@cancellar.hu>
2016-03-30 07:16:59 -0700

    corrected boost build scripts: only build filesystem and system, and copy headers

--- build_boost.bat
+++ build_boost.bat
@@ -1,4 +1,5 @@
 cd vendor\boost
 git submodule update --init --recursive
 bootstrap.bat
-b2 headers install link=static runtime-link=shared threading=multi --prefix=../boost-install/
+b2 install link=static runtime-link=shared threading=multi --prefix=../boost-install/ --with-filesystem --with-system
+b2 headers link=static runtime-link=shared threading=multi --prefix=../boost-install/
--- build_boost.sh
+++ build_boost.sh
@@ -2,4 +2,5 @@
 cd vendor/boost
 git submodule update --init --recursive
 ./bootstrap.sh
-./b2 install headers link=static runtime-link=shared threading=multi --prefix=../boost-install/
+./b2 install link=static runtime-link=shared threading=multi --prefix=../boost-install/ --with-filesystem --with-system
+./b2 headers --prefix=../boost-install/
