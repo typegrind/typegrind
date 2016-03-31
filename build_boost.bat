f93dddba6b31af5cf2c4205e9980619a11c3394e
Zsolt Parragi <zsolt.parragi@cancellar.hu>
2016-03-31 08:32:05 -0700

    more boost changes: calling b2 headers before install

--- build_boost.bat
+++ build_boost.bat
@@ -1,5 +1,5 @@
 cd vendor\boost
 git submodule update --init --recursive
 bootstrap.bat
+b2 headers 
 b2 install link=static runtime-link=shared threading=multi --prefix=../boost-install/ --with-filesystem --with-system
-b2 headers link=static runtime-link=shared threading=multi --prefix=../boost-install/
--- build_boost.sh
+++ build_boost.sh
@@ -2,5 +2,5 @@
 cd vendor/boost
 git submodule update --init --recursive
 ./bootstrap.sh
+./b2 headers
 ./b2 install link=static runtime-link=shared threading=multi --prefix=../boost-install/ --with-filesystem --with-system
-./b2 headers --prefix=../boost-install/
