d3bae4a425d542f70e9bed12f06a143cc3b7bf31
Zsolt Parragi <zsolt.parragi@cancellar.hu>
2016-04-02 18:54:03 +0000

    refactored operator new, modified tests to match it

--- src/common/handler/OpNewExprHandler.cpp
+++ src/common/handler/OpNewExprHandler.cpp
@@ -7,7 +7,7 @@
 namespace typegrind
 {
   OpNewExprHandler::OpNewExprHandler(clang::Rewriter*& rewriter)
-          : mRewriter(rewriter)
+          : BaseExprHandler(rewriter)
   {
   }
 
@@ -16,102 +16,54 @@ namespace typegrind
     const clang::ExplicitCastExpr* castExpr = result.Nodes.getNodeAs<clang::ExplicitCastExpr>("castExpr");
     const clang::CallExpr* newExpr = result.Nodes.getNodeAs<clang::CallExpr>("newStmt");
     const clang::FunctionDecl* funDecl = result.Nodes.getNodeAs<clang::FunctionDecl>("fun");
-    if (nullptr == castExpr)
-    {
-      llvm::errs() << "Couldn't convert MatcherResult to ExplicitCastExpr!\n";
-      return;
-    }
+
     if (nullptr == newExpr)
     {
       llvm::errs() << "Couldn't convert MatcherResult to CallExpr!\n";
       return;
     }
 
-    auto& sm = result.Context->getSourceManager();
 
-    clang::SourceLocation startLoc = newExpr->getLocStart();
-    // only instrument a source location once
-    if(mAlreadyEncoded.find(startLoc.getRawEncoding()) != mAlreadyEncoded.end()) return;
-    mAlreadyEncoded.insert(startLoc.getRawEncoding());
+    clang::SourceLocation startLoc = newExpr->getArg(0)->getLocStart();
+    clang::SourceLocation endLoc = newExpr->getArg(0)->getLocEnd();
+
+    if (!processingLocation(startLoc)) return;
 
 
-    // Skipping substituted template types
-    auto srcType = castExpr->getTypeInfoAsWritten()->getTypeLoc();
-    auto ptr = srcType.getType().getTypePtr()->getAs<clang::SubstTemplateTypeParmType>();
-    if (ptr)
+    MacroAdder newLoggerMacro(
+            funDecl->getNameInfo().getName().getAsString()=="operator new[]" ? "TYPEGRIND_LOG_OP_NEW_ARRAY" : "TYPEGRIND_LOG_OP_NEW",
+            startLoc,
+            endLoc,
+            mRewriter
+    );
+
+
+    if (castExpr)
     {
-      // This is an automatic template specialization.
-      // TODO: add additional checks that this condition is good! TBH I have no idea why it works, I should read more CLANG apidocs :)
-      // I mean:
-      // * partial template specialization
-      // * why isn't a specialized template a substitution? (or maybe it's in the standard?)
-      return;
-    }
+      auto srcType = castExpr->getTypeInfoAsWritten()->getTypeLoc().getType();
+      auto ptr = srcType.getTypePtr()->getAs<clang::SubstTemplateTypeParmType>();
+      // Skipping substituted template types
+      if (ptr)
+      {
+        // This is an automatic template specialization.
+        // TODO: better condition? TEST IT!
+        return;
+      }
 
-    std::string macroStart = "TYPEGRIND_LOG_OP_NEW";
-    if (funDecl->getNameInfo().getName().getAsString()=="operator new[]")
-    {
-      macroStart += "_ARRAY";
-    }
+      addTypeInformationParameters(newLoggerMacro, srcType);
 
-    macroStart += "(";
-
-    // 1st paramter: name of the type.
-    // Currently it is the concrete name if we can place it in the source without generating a template specialization
-    // otherwise it's based on typeid, so we are missing a demangle function ...
-
-    auto allocatedType = castExpr->getType()->getPointeeType();
-    if (allocatedType.getTypePtr()->isTemplateTypeParmType())
-    {
-      // TODO: somehow add compiler specific demangle ...
-      // We could generate a (manual) specialization for this function, but for now, this is enough
-      // Let's just use type_info::name(), and demangle it somehow runtime...
-      // or maybe create an initialization code dumping the mangled names for all affected specializations? (the ones skipped by the previous return)
-      macroStart += "TYPEGRIND_DEMANGLE(typeid(" + allocatedType.getAsString() + ").name())";
+      auto allocatedType = castExpr->getType()->getPointeeType();
+      // 4th parameter: sizeof type
+      newLoggerMacro.addParameter("sizeof(" + allocatedType.getAsString() + ")");
     }
     else
     {
-      macroStart += "\"" + allocatedType.getAsString() + "\""; // + (int)allocatedType.getTypePtr()->getAsTagDecl();
+      // no idea about the type
+      newLoggerMacro.addParameterAsString("void");
+      newLoggerMacro.addParameterAsString("void");
+      newLoggerMacro.addParameter("0");
     }
-    macroStart += ", ";
 
-    // 2nd parameter: source location. At least this is easy
-    auto ploc = sm.getPresumedLoc(newExpr->getLocEnd());
-    macroStart += "\"";
-    macroStart += ploc.getFilename();
-    macroStart += ":";
-    macroStart += std::to_string(ploc.getLine());
-    macroStart += "\", ";
-
-
-
-    // 4th parameter: sizeof type
-    macroStart += "sizeof(";
-    macroStart += allocatedType.getAsString();
-    macroStart += ")";
-    macroStart += ", ";
-
-
-    // 4th argument: size, which is the first argument to the call
-    // TODO: extract it to a variable!
-    llvm::raw_string_ostream os(macroStart);
-    newExpr->getArg(0)->printPretty(os, nullptr, clang::PrintingPolicy(result.Context->getPrintingPolicy()));
-    os.flush();
-    macroStart += ", ";
-
-    // 4/5th parameter: the new call. It's the expression itself
-
-    // end added function call
-    std::string macroEnd = ")";
-
-    mRewriter->InsertText(startLoc, macroStart);
-
-    clang::SourceLocation endLoc = newExpr->getLocEnd();
-    mRewriter->InsertTextAfterToken(endLoc, macroEnd);
-  }
-
-  clang::StringRef OpNewExprHandler::getID() const
-  {
-    return "typegrind";
+    newLoggerMacro.commit();
   }
 }
