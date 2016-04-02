322482520225348a9e67fce0dffab360eac00e2f
Zsolt Parragi <zsolt.parragi@cancellar.hu>
2016-04-02 09:26:06 +0000

    added more tests about new and operator new

---
+++ test/array_new.cpp
@@ -0,0 +1,10 @@
+// RUN: mkdir -p %T/Inputs/
+// RUN: clang-typegrind %s --
+// RUN: sed -i 's/\/\/.*//' %T/Inputs/array_new.cpp 
+// RUN: FileCheck -input-file=%T/Inputs/array_new.cpp %s
+
+int main(void) {
+    // CHECK: int* pT = TYPEGRIND_LOG_NEW_ARRAY("int", "{{.*}}/clang-typegrind/test/array_new.cpp:8", sizeof(int), 53, new int[53]);
+    int* pT = new int[53];
+    return 0;
+}
