fd6bcf1981dcec0c0f4872e4084a7028dc45d955
Zsolt Parragi <zsolt.parragi@cancellar.hu>
2016-04-02 11:58:02 +0000

    code cleanup: indentation

--- include/DirectoryMapper.h
+++ include/DirectoryMapper.h
@@ -9,8 +9,8 @@ namespace typegrind
   class DirectoryMapper
   {
   public:
-      void add(std::string from, std::string to);
-      bool apply(std::string& path) const; // returns = found a mapping
+    void add(std::string from, std::string to);
+    bool apply(std::string& path) const; // returns = found a mapping
 
   private:
     std::vector<DirectoryMapping> mappings;
