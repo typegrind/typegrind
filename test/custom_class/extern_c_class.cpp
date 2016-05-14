862e7f7e8f495da9e8a9ab1deb85470b6855ee39
Zsolt Parragi <zsolt.parragi@cancellar.hu>
2016-05-14 17:17:16 +0200

    fixed a broken test

--- test/custom_class/extern_c_class.cpp
+++ test/custom_class/extern_c_class.cpp
@@ -1,21 +1,16 @@
-:colorscheme murphy
-
-
-
-
 // RUN: clang-typegrind %s --
 // RUN: sed -i 's/\/\/.*//' %T/../../Output/Inputs/custom_class/extern_c_class.cpp
 // RUN: FileCheck -input-file=%T/../../Output/Inputs/custom_class/extern_c_class.cpp %s
 
 extern "C" {
 class C {
-// CHECK: TYPEGRIND_RECORD_APPEND_C("{{.*}}/clang-typegrind/test/custom_class/extern_c_class.cpp:9")};
+// CHECK: TYPEGRIND_RECORD_APPEND_C("{{.*}}/clang-typegrind/test/custom_class/extern_c_class.cpp:8")};
 };
 }
 
 int main(void)
 {
-  // CHECK: C* pT = TYPEGRIND_LOG_NEW("{{.*}}/clang-typegrind/test/custom_class/extern_c_class.cpp:15", "class C", "class C", (new C()), sizeof(C));
+  // CHECK: C* pT = TYPEGRIND_LOG_NEW("{{.*}}/clang-typegrind/test/custom_class/extern_c_class.cpp:14", "class C", "class C", (new C()), sizeof(C));
   C* pT = new C();
   return 0;
 }
